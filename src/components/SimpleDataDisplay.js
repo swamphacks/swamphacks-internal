6553053b7ededeff3e02b6decf25d216fd209d46
Zachary Cowan <cz8ch8ry@gmail.com>
2020-02-01 15:16:49 -0500

    Fixed minor visual bug

--- src/components/SimpleDataDisplay.js
+++ src/components/SimpleDataDisplay.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 // Material UI
-import {makeStyles} from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import Paper from '@material-ui/core/Paper';
 import CircularProgress from '@material-ui/core/CircularProgress';
@@ -12,7 +12,8 @@ const useStyles = makeStyles(theme => ({
     display: 'flex',
     justifyContent: 'center',
     flexDirection: 'column',
-    padding: 20
+    padding: 20,
+    height: '100%'
   },
   progressContainer: {
     display: 'flex',
@@ -22,7 +23,7 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
-const SimpleDataDisplay = ({label, data}) => {
+const SimpleDataDisplay = ({ label, data }) => {
   const classes = useStyles();
   return (
     <Paper className={classes.root}>
